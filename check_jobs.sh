ff9683fa0f35a646a7a5684c0a181da38410caf8
Archishman Sravankumar <archishman@gmail.com>
2021-08-07 15:22:12 -0700

    initial commit

---
+++ check_jobs.sh
@@ -0,0 +1,21 @@
+function handle_changed {
+    echo '[' $(date -u) '] Files have changed' >> log.txt;
+    rm -rf .temp
+    mv .temp2 .temp
+    rm -rf .temp2
+    curl -d "app_key=$APP_KEY&app_secret=$APP_SECRET&content=Positions%20Updated&target_type=app" -X POST https://api.pushed.co/1/push
+}
+function handle_unchanged {
+    echo '[' $(date -u) '] Files are identical' >> log.txt
+    rm -rf .temp2
+}
+function check_updates {
+    git ls-remote https://github.com/coderQuad/New-Grad-Positions-2022.git | head -n 1 | awk '{print $1;}' > ./.temp2
+}
+export $(cat .env | xargs)
+while true
+do
+    check_updates
+    cmp --silent .temp .temp2 && handle_unchanged || handle_changed;
+    sleep 5;
+done
